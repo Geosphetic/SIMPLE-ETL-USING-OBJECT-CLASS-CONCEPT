50ae4188ba83f83402cb12bdbcf317169d05bd67
Kapur Mahatara <kapur.mahatara@pdc01.lisnepal.com.np>
2023-11-29 10:42:57 +0545

    Changed the connect.py

--- connection/connect.py
+++ connection/connect.py
@@ -2,9 +2,9 @@ import snowflake.connector
 
 def connection():
     con = snowflake.connector.connect(
-    user = 'samipth',
-    password = 'LisNepal123',
-    account = 'wuyamtt-iw98182',
+    user = 'YourSnowflakeID',
+    password = 'YourSnowflake Password',
+    account = 'Your Snowflake Account', #in URL
     database = 'BHATBHATENI_DB',
     source_schema = 'BHATBHATENI_DB.TRANSACTIONS',
     staging_schema = 'BHATBHATENI_DB.STG',
